3fe2878eae5baaff61547ed2ef712045a2fb7856
shyam <shyam.amratiya@advancedstructures.in>
2019-06-11 23:55:53 +0530

    button function commented

--- interview.js
+++ interview.js
@@ -1,33 +1,33 @@
-var list1,list2;
-function addToList2(){
-  list1=document.querySelectorAll('#list1 li');
-  if(list1){
-    for(let i=0;i<list1.length;i++){
-      list2=document.querySelector('#list2').append(list1[i]);
-    }
+// var list1,list2;
+// function addToList2(){
+//   list1=document.querySelectorAll('#list1 li');
+//   if(list1){
+//     for(let i=0;i<list1.length;i++){
+//       list2=document.querySelector('#list2').append(list1[i]);
+//     }
     
-  }
-}
-function addToList1(){
-  list2=document.querySelectorAll('#list2 li');
-  if(list2){
-    for(let i=0;i<list2.length;i++){
-    list1=document.querySelector('#list1').appendChild(list2[i]);
-  }
-} 
-}
-function addOneElementToList2(){
-  list1=document.querySelector('#list1 li');
-  if(list1){
-      list2=document.querySelector('#list2').append(list1);
-  }
-}
-function addOneElementToList1(){
-  list2=document.querySelector('#list2 li');
-  if(list2){
-    list1=document.querySelector('#list1').appendChild(list2);
-} 
-}
+//   }
+// }
+// function addToList1(){
+//   list2=document.querySelectorAll('#list2 li');
+//   if(list2){
+//     for(let i=0;i<list2.length;i++){
+//     list1=document.querySelector('#list1').appendChild(list2[i]);
+//   }
+// } 
+// }
+// function addOneElementToList2(){
+//   list1=document.querySelector('#list1 li');
+//   if(list1){
+//       list2=document.querySelector('#list2').append(list1);
+//   }
+// }
+// function addOneElementToList1(){
+//   list2=document.querySelector('#list2 li');
+//   if(list2){
+//     list1=document.querySelector('#list1').appendChild(list2);
+// } 
+// }
 
 function drag(ev) {
   ev.target.style.border='3px dashed #ebebeb';
